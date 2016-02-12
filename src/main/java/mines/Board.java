ff93dd5fd4b41ca7c77bcf3a6a66975bb2bf4921
kate-young <S1391628@student.mcckc.edu>
2016-02-11 21:26:31 -0600

    Initial Commit

---
+++ src/main/java/mines/Board.java
@@ -0,0 +1,22 @@
+package mines;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by KY025863 on 2/11/2016.
+ */
+public class Board {
+    private final Map<int[], Field> fields;
+    private final int rows;
+    private final int columns;
+
+    public Board(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+        this.fields = new HashMap<>();
+        for(int r = 0; r < rows; r++)
+            for (int c = 0; c < columns; c++)
+                fields.put(new int[] {r, c}, new Field(r, c));
+    }
+}
---
+++ src/main/java/mines/Field.java
@@ -0,0 +1,14 @@
+package mines;
+
+/**
+ * Created by KY025863 on 2/11/2016.
+ */
+public class Field {
+    private int row;
+    private int column;
+
+    public Field(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+}
